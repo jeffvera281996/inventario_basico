45cd9fe444bf2339a6d54e9727ad60afafd3ef1e
Jefferson Ricardo Vera Macas <jeffvera281996@gmail.com>
2026-09-02 14:58:26 -0500

    actualizacion conexion de la base de datos
    actualizacion conexion de la base de datos

---
+++ config/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host      = "localhost";
+$usuario   = "root";
+$clave     = "";
+$basedatos = "integradora";
+
+$conexion = new mysqli($host, $usuario, $clave, $basedatos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión a la base de datos: " . $conexion->connect_error);
+}
+
+$conexion->set_charset("utf8");
